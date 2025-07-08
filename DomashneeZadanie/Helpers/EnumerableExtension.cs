a51a50e1f3d9ef429355a8746db94102642ffc26
AD <yra193@yandex.ru>
2025-07-08 21:05:28 +0300

    dz13_2

--- DomashneeZadanie/Helpers/EnumerableExtension.cs
+++ DomashneeZadanie/Helpers/EnumerableExtension.cs
@@ -6,22 +6,14 @@ namespace Domashneezadanie.Helpers
 {
     public static class EnumerableExtension
     {
-        /// <summary>
-        /// Возвращает подмножество элементов из последовательности, соответствующее указанной "пачке".
-        /// </summary>
-        /// <typeparam name="T">Тип элементов последовательности.</typeparam>
-        /// <param name="source">Исходная последовательность.</param>
-        /// <param name="batchSize">Размер пачки (количество элементов в одной пачке).</param>
-        /// <param name="batchNumber">Номер пачки (нумерация с 0).</param>
-        /// <returns>Подмножество элементов из последовательности.</returns>
         public static IEnumerable<T> GetBatchByNumber<T>(this IEnumerable<T> source, int batchSize, int batchNumber)
         {
             if (source == null)
                 throw new ArgumentNullException(nameof(source));
             if (batchSize <= 0)
-                throw new ArgumentOutOfRangeException(nameof(batchSize), "Размер пачки должен быть больше 0.");
+                throw new ArgumentOutOfRangeException(nameof(batchSize), "Размер пачки не может быть 0.");
             if (batchNumber < 0)
-                throw new ArgumentOutOfRangeException(nameof(batchNumber), "Номер пачки не может быть отрицательным.");
+                throw new ArgumentOutOfRangeException(nameof(batchNumber), "Номер пачки не может быть отрицательнымю");
 
             return source.Skip(batchSize * batchNumber).Take(batchSize);
         }
